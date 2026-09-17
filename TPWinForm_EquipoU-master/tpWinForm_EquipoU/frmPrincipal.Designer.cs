050df5b71de8c9311d78910660f083dea0c30fef
LeandroEze2004 <mikolajewski.04@gmail.com>
2026-09-17 12:23:58 -0300

    Conexión SQL al VisualStudios + Intente poner lo de la Imagen pero algunas no me cargan (Consultar al profesor).

---
+++ TPWinForm_EquipoU-master/tpWinForm_EquipoU/frmPrincipal.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace tpWinForm_EquipoU
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArticulos = new System.Windows.Forms.Button();
+            this.btnMarcas = new System.Windows.Forms.Button();
+            this.btnCategorias = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblMenuPrincipal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnArticulos
+            // 
+            this.btnArticulos.Location = new System.Drawing.Point(45, 77);
+            this.btnArticulos.Name = "btnArticulos";
+            this.btnArticulos.Size = new System.Drawing.Size(75, 23);
+            this.btnArticulos.TabIndex = 0;
+            this.btnArticulos.Text = "Articulos";
+            this.btnArticulos.UseVisualStyleBackColor = true;
+            this.btnArticulos.Click += new System.EventHandler(this.btnArticulos_Click);
+            // 
+            // btnMarcas
+            // 
+            this.btnMarcas.Location = new System.Drawing.Point(160, 77);
+            this.btnMarcas.Name = "btnMarcas";
+            this.btnMarcas.Size = new System.Drawing.Size(75, 23);
+            this.btnMarcas.TabIndex = 1;
+            this.btnMarcas.Text = "Marcas";
+            this.btnMarcas.UseVisualStyleBackColor = true;
+            this.btnMarcas.Click += new System.EventHandler(this.btnMarcas_Click);
+            // 
+            // btnCategorias
+            // 
+            this.btnCategorias.Location = new System.Drawing.Point(275, 77);
+            this.btnCategorias.Name = "btnCategorias";
+            this.btnCategorias.Size = new System.Drawing.Size(75, 23);
+            this.btnCategorias.TabIndex = 2;
+            this.btnCategorias.Text = "Categorias";
+            this.btnCategorias.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(384, 77);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // lblMenuPrincipal
+            // 
+            this.lblMenuPrincipal.AutoSize = true;
+            this.lblMenuPrincipal.BackColor = System.Drawing.Color.LightSlateGray;
+            this.lblMenuPrincipal.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenuPrincipal.Location = new System.Drawing.Point(133, 9);
+            this.lblMenuPrincipal.Name = "lblMenuPrincipal";
+            this.lblMenuPrincipal.Size = new System.Drawing.Size(241, 32);
+            this.lblMenuPrincipal.TabIndex = 4;
+            this.lblMenuPrincipal.Text = "MENU PRINCIPAL";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 130);
+            this.Controls.Add(this.lblMenuPrincipal);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCategorias);
+            this.Controls.Add(this.btnMarcas);
+            this.Controls.Add(this.btnArticulos);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal (menu)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArticulos;
+        private System.Windows.Forms.Button btnMarcas;
+        private System.Windows.Forms.Button btnCategorias;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblMenuPrincipal;
+    }
+}
+
