ac9652271aa68646dbdf98d999219db38c6399e4
mauricio-pla <mauripla7@gmail.com>
2026-09-16 21:32:52 -0300

    crear ventana articulo.

---
+++ tpWinForm_EquipoU/frmArticulos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpWinForm_EquipoU
+{
+    public partial class frmArticulos : Form
+    {
+        public frmArticulos()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvArticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmArticulos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
